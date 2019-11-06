ec5fcf27b188d3aa9b93c4e331a231d33556cfb8
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2019-11-06 10:09:35 -0500

    Added library/test for edge flow visualization

--- src/visualization/Visualization.java
+++ src/visualization/Visualization.java
@@ -1,8 +1,8 @@
 package visualization;
 
-import org.graphstream.algorithm.Toolkit;
-import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.spriteManager.Sprite;
+import org.graphstream.ui.spriteManager.SpriteManager;
 import org.graphstream.ui.swingViewer.ViewPanel;
 import org.graphstream.ui.view.View;
 import org.graphstream.ui.view.Viewer;
@@ -19,8 +19,13 @@ public class Visualization implements ViewerListener {
     private boolean loop = true;
 
     private void run() {
+        System.setProperty("org.graphstream.ui.renderer",
+                "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+
         SingleGraph graph = new SingleGraph("g1");
         Viewer viewer = graph.display();
+
+        SpriteManager sman = new SpriteManager(graph);
 
         View view = viewer.getDefaultView();
         for (MouseListener listener : ((ViewPanel) view).getMouseListeners()) {
@@ -32,67 +37,50 @@ public class Visualization implements ViewerListener {
         pipeIn.addSink(graph);
         pipeIn.addViewerListener(this);
 
-        graph.setAttribute("ui.stylesheet", styleSheet);
+        graph.setAttribute("ui.stylesheet", "sprite { shape: flow; size: 5px;" +
+                " z-index: 0; } sprite#S1 { fill-color: #373; } sprite#S2 { " +
+                "fill-color: #393; } sprite#S3 { fill-color: #3B3; }");
         graph.setAttribute("ui.antialias");
         graph.setAttribute("ui.quality");
 
-        Node root = graph.addNode("root");
-        Node A = graph.addNode("A");
-        Node B = graph.addNode("B");
-        Node C = graph.addNode("C");
-        Node D = graph.addNode("D");
-        Node E = graph.addNode("E");
-        Node F = graph.addNode("F");
-        Node G = graph.addNode("G");
-        Node H = graph.addNode("H");
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("AB", "A", "B");
+        graph.addEdge("BC", "B", "C");
+        graph.addEdge("CA", "C", "A");
+        graph.getNode("A").setAttribute("xyz", new double[]{-1, 0, 0});
+        graph.getNode("B").setAttribute("xyz", new double[]{1, 0, 0});
+        graph.getNode("C").setAttribute("xyz", new double[]{0, 1, 0});
 
-        graph.addEdge("rA", "root", "A");
-        graph.addEdge("rB", "root", "B");
-        graph.addEdge("rC", "root", "C");
-        graph.addEdge("rD", "root", "D");
-        graph.addEdge("rE", "root", "E");
-        graph.addEdge("AF", "A", "F");
-        graph.addEdge("CG", "C", "G");
-        graph.addEdge("DH", "D", "H");
+        SpriteManager sm = new SpriteManager(graph);
+        Sprite s1 = sm.addSprite("S1");
+        Sprite s2 = sm.addSprite("S2");
+        Sprite s3 = sm.addSprite("S3");
 
-        root.setAttribute("xyz", new double[]{0, 0, 0});
-        A.setAttribute("xyz", new double[]{1, 1, 0});
-        B.setAttribute("xyz", new double[]{1, 0, 0});
-        C.setAttribute("xyz", new double[]{-1, 1, 0});
-        D.setAttribute("xyz", new double[]{-1, 0, 0});
-        E.setAttribute("xyz", new double[]{-1, -1, 0});
-        F.setAttribute("xyz", new double[]{2, 1.2, 0});
-        G.setAttribute("xyz", new double[]{-2, 1.2, 0});
-        H.setAttribute("xyz", new double[]{-2, -.5, 0});
+        s1.attachToEdge("AB");
+        s2.attachToEdge("BC");
+        s3.attachToEdge("CA");
+        s1.setPosition(0);
+        s2.setPosition(0);
+        s3.setPosition(0);
 
-        root.setAttribute("label", "Idea");
-        A.setAttribute("label", "Topic1");
-        B.setAttribute("label", "Topic2");
-        C.setAttribute("label", "Topic3");
-        D.setAttribute("label", "Topic4");
-        E.setAttribute("label", "Topic5");
-        F.setAttribute("label", "SubTopic1");
-        G.setAttribute("label", "SubTopic2");
-        H.setAttribute("label", "Very Long Sub Topic ...");
+        double s1pos = 0, s2pos = 0, s3pos = 0;
 
         int i = 1;
 
         while (loop) {
             pipeIn.pump();
-            sleep(1000);
+            sleep(500);
 
-            Node newNode = graph.addNode(Integer.toString(i));
-
-            String parent = Integer.toString(i / 2);
-            if (parent.equals("0")) {
-                parent = "E";
+            if (s1pos < 1.0) {
+                s1pos += 0.1;
+                s2pos += 0.1;
+                s3pos += 0.1;
+                s1.setPosition(s1pos);
+                s2.setPosition(s2pos);
+                s3.setPosition(s3pos);
             }
-
-            newNode.setAttribute("label", Integer.toString(i));
-            newNode.setAttribute("xyz",
-                    Toolkit.nodePosition(graph.getNode(parent)));
-            graph.addEdge(parent + "," + i, parent, Integer.toString(i));
-            i++;
         }
 
         System.out.println("bye bye");
@@ -164,6 +152,11 @@ public class Visualization implements ViewerListener {
             + "	shadow-width: 0px;"
             + "	arrow-shape: arrow;"
             + "	arrow-size: 20px, 6px;"
+            + "}"
+            + ""
+            + "sprite {"
+            + " shape: flow;"
+            + " fill-color: green;"
             + "}";
 
     public void mouseOver(String id) {}
