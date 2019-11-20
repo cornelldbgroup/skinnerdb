aa94ce2cc8711187680b31031dc2900109d81f13
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2019-11-20 10:50:36 -0500

    Made TPCH have the same dir structure as IMDB

--- tpch/skinner.load.sql
+++ tpch/skinner.load.sql
@@ -1,9 +1,9 @@
-load lineitem /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/lineitem.csv | NULL;
-load nation /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/nation.csv | NULL;
-load part /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/part.csv | NULL;
-load partsupp /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/partsupp.csv | NULL;
-load supplier /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/supplier.csv | NULL;
-load region /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/region.csv | NULL;
-load orders /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/orders.csv | NULL;
-load customer /Users/immanueltrummer/Data/tpch/tpc-h-sf-1/customer.csv | NULL;
+load lineitem tpch/data/lineitem.csv | NULL;
+load nation tpch/data/nation.csv | NULL;
+load part tpch/data/part.csv | NULL;
+load partsupp tpch/data/partsupp.csv | NULL;
+load supplier tpch/data/supplier.csv | NULL;
+load region tpch/data/region.csv | NULL;
+load orders tpch/data/orders.csv | NULL;
+load customer tpch/data/customer.csv | NULL;
 compress;
