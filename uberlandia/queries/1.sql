50f812b14f435983bf4bace2ca6dd25f83bc4719
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-01-23 13:35:42 -0500

    Started adding uberlandia database for testing predicate switching

---
+++ uberlandia/queries/1.sql
@@ -0,0 +1,1 @@
+SELECT "uberlandia_1"."Calculation_838513978982854656", "uberlandia_1"."municipio_do_local_da_oferta" FROM "uberlandia_1";
---
+++ uberlandia/skinner.load.sql
@@ -0,0 +1,2 @@
+load Uberlandia_1 data/Uberlandia_1.sample.csv , NULL;
+compress;
