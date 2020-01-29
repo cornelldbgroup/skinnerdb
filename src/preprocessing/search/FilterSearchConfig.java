faedde42c19f63a1c793b4741f22892208837263
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-01-29 15:41:39 -0500

    More exploration with the exploration factor

--- src/preprocessing/search/FilterSearchConfig.java
+++ src/preprocessing/search/FilterSearchConfig.java
@@ -2,5 +2,5 @@ package preprocessing.search;
 
 public class FilterSearchConfig {
     public static final long BUDGET = 1000000;
-    public static final double EXPLORATION_FACTOR = 1e-5;
+    public static final double EXPLORATION_FACTOR = 1e-6;
 }
