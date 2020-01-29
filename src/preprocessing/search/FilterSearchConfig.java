cabc9527e77e3e0a61822fb2372fb9eaa716fc01
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-01-29 15:13:41 -0500

    1e-5 factor

--- src/preprocessing/search/FilterSearchConfig.java
+++ src/preprocessing/search/FilterSearchConfig.java
@@ -2,5 +2,5 @@ package preprocessing.search;
 
 public class FilterSearchConfig {
     public static final long BUDGET = 1000000;
-    public static final double EXPLORATION_FACTOR = 1.0 / Math.sqrt(2);
+    public static final double EXPLORATION_FACTOR = 1e-5;
 }
