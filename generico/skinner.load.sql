2249e9a492a93f66954f01a483a632657892812f
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-02-07 14:42:29 -0500

    Added schema and load commands

---
+++ generico/skinner.load.sql
@@ -0,0 +1,6 @@
+load generico_1 generico/data/generico_1.csv | null;
+load generico_2 generico/data/generico_2.csv | null;
+load generico_3 generico/data/generico_3.csv | null;
+load generico_4 generico/data/generico_4.csv | null;
+load generico_5 generico/data/generico_5.csv | null;
+compress;
