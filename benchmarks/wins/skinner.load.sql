f4e7744538cc697ec5573c3edd626d7bc4b647f5
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-02-08 14:54:21 -0500

    Added wins database

---
+++ benchmarks/wins/skinner.load.sql
@@ -0,0 +1,5 @@
+load wins_1 benchmarks/wins/data/wins_1.csv | null;
+load wins_2 benchmarks/wins/data/wins_2.csv | null;
+load wins_3 benchmarks/wins/data/wins_3.csv | null;
+load wins_4 benchmarks/wins/data/wins_4.csv | null;
+compress;
