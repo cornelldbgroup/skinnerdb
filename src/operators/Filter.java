370473a13d69c76b11e28c3b0465c679e521e3db
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-02-15 18:47:55 -0500

    Disabled parallel processing for BasicPreprocessor

--- src/operators/Filter.java
+++ src/operators/Filter.java
@@ -98,7 +98,7 @@ public class Filter {
         // Initialize filter result
         IntList result = null;
         // Choose between sequential and parallel processing
-        if (cardinality <= ParallelConfig.PRE_BATCH_SIZE) {
+        if (true || cardinality <= ParallelConfig.PRE_BATCH_SIZE) {
             RowRange allTuples = new RowRange(0, cardinality - 1);
             result = filterBatch(unaryBoolEval, allTuples);
         } else {
