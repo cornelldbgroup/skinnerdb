3f58a8750b9e1fc3933be4ca0f71f207b7684df2
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-02-21 20:54:21 +0000

    Revert "Shutdown thread pool without waiting"
    This reverts commit 1f51e6289e1c74ad5a160c6ed8f18099d3d488ac.

--- src/preprocessing/search/SearchPreprocessor.java
+++ src/preprocessing/search/SearchPreprocessor.java
@@ -123,7 +123,7 @@ public class SearchPreprocessor implements Preprocessor {
         for (Future f : futures) {
             f.get();
         }
-        threadPool.shutdownNow();
+        threadPool.shutdown();
 
         // Abort pre-processing if filtering error occurred
         if (hadError) {
