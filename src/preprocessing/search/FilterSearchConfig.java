a5501e6c9eb313e57f8cec972e8a1f75a3f8059a
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-02-24 08:06:49 -0800

    Revert "Hyper parameter toying"
    This reverts commit b17805ac564b2f7d40541a02bb875e4be6358197.

--- src/preprocessing/search/FilterSearchConfig.java
+++ src/preprocessing/search/FilterSearchConfig.java
@@ -2,8 +2,8 @@ package preprocessing.search;
 
 public class FilterSearchConfig {
     public static final int ROWS_PER_TIMESTEP = 1000;
-    public static final int PARALLEL_ROWS_PER_TIMESTEP = 15000;
-    public static final double EXPLORATION_FACTOR = 1e-7;
+    public static final int PARALLEL_ROWS_PER_TIMESTEP = 10000;
+    public static final double EXPLORATION_FACTOR = 1e-5;
     public static final int BRANCHING_PARALLEL_ACTIONS = 4;
     public static boolean FORGET = true;
 }
