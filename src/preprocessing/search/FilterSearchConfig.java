30408cb61dffc25447434cf03ea0d280c827aca8
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-02-25 02:32:58 +0000

    Reset hyper parameters to original config

--- src/preprocessing/search/FilterSearchConfig.java
+++ src/preprocessing/search/FilterSearchConfig.java
@@ -5,7 +5,7 @@ public class FilterSearchConfig {
     public static final int PARALLEL_ROWS_PER_TIMESTEP = 10000;
     public static final double EXPLORATION_FACTOR = 1e-5;
     public static final int ROW_PARALLEL_ACTIONS = 4;
-    public static final int ROW_PARALLEL_DELTA = 10;
+    public static final int ROW_PARALLEL_DELTA = 5;
     public static final boolean ENABLE_ROW_PARALLELISM = true;
     public static final boolean ENABLE_COMPILATION = true;
     public static final boolean FORGET = true;
