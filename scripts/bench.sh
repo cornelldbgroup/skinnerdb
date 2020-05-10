0c1eee267481b98f904a6a1c83c85b024c8e6009
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2020-05-10 10:23:53 -0700

    Fix bench script to handle index parameter

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -14,6 +14,10 @@ rm -f $2"${DB}"*
 for i in {1..5}
 do
     echo "Starting: $i"
-    printf 'bench %s %s 3\nquit' $1 "$2${DB}-$i.log" | $DIR/skinnerdb $3
+    if [ $INDEX ]; then
+        printf 'index all;\nbench %s %s 3\nquit' $1 "$2${DB}-$i.log" | $DIR/skinnerdb $3
+    else
+        printf 'index all;\nbench %s %s 3\nquit' $1 "$2${DB}-$i.log" | $DIR/skinnerdb $3
+    fi 
     echo "Finished: $i"
 done
