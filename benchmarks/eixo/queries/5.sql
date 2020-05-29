fd458bd4d9bc34696bcb2a60522f45330ece8ada
Ankush Rayabhari <ar2354@cornell.edu>
2020-05-29 11:38:13 -0700

    Removed postprocessing from eixo

--- benchmarks/eixo/queries/5.sql
+++ benchmarks/eixo/queries/5.sql
@@ -1,1 +1,1 @@
-SELECT eixo_1.nome_da_sit_matricula_situacao_detalhada AS nome_da_sit_matricula_situacao_detalhada FROM eixo_1 GROUP BY eixo_1.nome_da_sit_matricula_situacao_detalhada LIMIT 28;
+SELECT eixo_1.nome_da_sit_matricula_situacao_detalhada AS nome_da_sit_matricula_situacao_detalhada FROM eixo_1;
---
+++ benchmarks/eixo/remove-postprocessing.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+find . -name "*.sql" | xargs perl -pi -e 's/[ ]?GROUP BY.*;/;/g'
+find . -name "*.sql" | xargs perl -pi -e 's/[ ]?HAVING.*;/;/g'
+find . -name "*.sql" | xargs perl -pi -e 's/SELECT.*FROM eixo_1 WHERE/SELECT eixo_1.number_of_records FROM eixo_1 WHERE/g'
+
